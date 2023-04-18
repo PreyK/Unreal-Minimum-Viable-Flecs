ef0ca9f6a6bc304466883cb88afb260240788c27
PreyK <krisitof@gmail.com>
2023-04-18 16:59:48 +0200

    blueprint design wip

---
+++ Source/FlecsTest/BP/FlecsSystem.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FlecsSystem.h"
+/*
+void UFlecsSystem::Native_Implementation()
+{
+}
+
+void UFlecsSystem::Callable()
+{
+}
+
+bool UFlecsSystem::Pure()
+{
+	return true;
+}
+*/
+
+UFlecsComponent* UFlecsEntity::GetComponent(TSubclassOf<UFlecsComponent> ComponentType)
+{
+	return nullptr;
+}
+
+void UFlecsSystem::RegisterSystemComponents(TArray<TSubclassOf<UFlecsComponent>> components)
+{
+}
---
+++ Source/FlecsTest/BP/FlecsSystem.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "FlecsSystem.generated.h"
+
+/**
+ * 
+ */
+
+
+
+UCLASS(Blueprintable)
+class UFlecsEntity : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	UFlecsComponent* GetComponent(TSubclassOf<UFlecsComponent> ComponentType);
+	
+};
+
+UCLASS(Blueprintable,BlueprintType)
+class UFlecsComponent : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	
+};
+
+
+UCLASS(Blueprintable)
+class FLECSTEST_API UFlecsSystem : public UObject
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnExecute(UFlecsEntity* CurrentEntity);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnInitialize();
+
+	
+	UFUNCTION(BlueprintCallable)
+	void RegisterSystemComponents(TArray<TSubclassOf<UFlecsComponent>> components);
+};
